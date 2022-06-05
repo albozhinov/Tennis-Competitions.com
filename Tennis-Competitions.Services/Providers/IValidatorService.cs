f56eade9f9adcf6363539180ed0b742a466dad51
albozhinov <a.bozhinov90@gmail.com>
2022-06-05 21:41:50 +0300

    Added validator service

---
+++ Tennis-Competitions.Services/Providers/IValidatorService.cs
@@ -0,0 +1,9 @@
+﻿namespace Tennis_Competitions.Services.Providers
+{
+    public interface IValidatorService
+    {
+        public void NullOrWhiteSpacesCheck(string parameter);
+
+        public (bool, Guid) TryParseGuid(string guidId);
+    }
+}
---
+++ Tennis-Competitions.Services/Providers/ValidatorService.cs
@@ -0,0 +1,29 @@
+﻿namespace Tennis_Competitions.Services.Providers
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ValidatorService : IValidatorService
+    {
+        public void NullOrWhiteSpacesCheck(string parameter)
+        {
+            if (String.IsNullOrEmpty(parameter) ||
+                String.IsNullOrWhiteSpace(parameter))
+            {
+                throw new ArgumentNullException("Nullable or white spaces values are not supported.");
+            }
+
+            return;
+        }
+
+        public (bool, Guid) TryParseGuid(string guidId)
+        {
+            Guid parsedArticleId;
+            bool isValidId = Guid.TryParse(guidId, out parsedArticleId);
+
+            if (!isValidId)
+                return (false, Guid.Empty);
+
+            return (true, parsedArticleId);
+        }
+    }
+}
