569cca1d272186cced53761897aea373ee756dba
albozhinov <a.bozhinov90@gmail.com>
2022-06-06 00:25:51 +0300

    Added jsons and LoadJsonInDB method in DBcontext.

--- Tennis-Competitions.Data/Context/TennisCompetitionsDBContext.cs
+++ Tennis-Competitions.Data/Context/TennisCompetitionsDBContext.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
     using Tennis_Competitions.Data.Models;
 
     public class TennisCompetitionsDBContext : IdentityDbContext
@@ -27,22 +28,45 @@
 
         }
 
+
+        private void LoadJsonInDB(ModelBuilder builder)
+        {
+            try
+            {
+                var tournamentsAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\tournaments.json");
+                var playersAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\players.json");
+                var matchesAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\matches.json");
+                var playerMatchAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\playermatch.json");
+                var playerTournamentAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\playertournament.json");
+
+                var tournaments = JsonConvert.DeserializeObject<Tournament[]>(tournamentsAsJson);
+                var players = JsonConvert.DeserializeObject<Player[]>(playersAsJson);
+                var matches = JsonConvert.DeserializeObject<Match[]>(matchesAsJson);
+                var playerMatches = JsonConvert.DeserializeObject<PlayerMatch[]>(playerMatchAsJson);
+                var playerTournaments = JsonConvert.DeserializeObject<PlayerTournament[]>(playerTournamentAsJson);
+
+                builder.Entity<Tournament>().HasData(tournaments);
+                builder.Entity<Player>().HasData(players);
+                builder.Entity<Match>().HasData(matches);
+                builder.Entity<PlayerMatch>().HasData(playerMatches);
+                builder.Entity<PlayerTournament>().HasData(playerTournaments);
+
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            LoadJsonInDB(builder);
+
             builder.Entity<PlayerMatch>()
                             .HasKey(pm => new { pm.PlayerId, pm.MatchId });
 
             builder.Entity<PlayerTournament>()
                             .HasKey(pt => new { pt.PlayerId, pt.TournamentId });
-
-            //builder.Entity<Player>().HasMany(p => p.PlayerMatches)
-            //    .WithOne(pm => pm.Player)
-            //    .HasForeignKey(pm => pm.MatchId)
-            //    .OnDelete(DeleteBehavior.Restrict);
-                                    
-
-
-
 
             base.OnModelCreating(builder);
         }
