72c9b80f4940a418cbf0aa3603356f53f511415e
albozhinov <a.bozhinov90@gmail.com>
2022-06-05 23:58:01 +0300

    Added new migration - AddedPlayersResults

---
+++ Tennis-Competitions.Data/Migrations/20220605205609_AddedPlayersResults.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tennis_Competitions.Data.Migrations
+{
+    public partial class AddedPlayersResults : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Matches");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstPlayerResult",
+                table: "Matches",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondPlayerResult",
+                table: "Matches",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstPlayerResult",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "SecondPlayerResult",
+                table: "Matches");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Matches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
