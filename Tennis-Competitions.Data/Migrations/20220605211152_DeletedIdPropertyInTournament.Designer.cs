bfbaeceea77456fd845b83fe6c2f65ba2caefd8e
albozhinov <a.bozhinov90@gmail.com>
2022-06-06 00:24:33 +0300

    Added new migration - DeleteIdInTournament

--- Tennis-Competitions.Data/Migrations/20220605211152_DeletedIdPropertyInTournament.Designer.cs
+++ Tennis-Competitions.Data/Migrations/20220605211152_DeletedIdPropertyInTournament.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tennis_Competitions.Data.Context;
 
@@ -11,9 +12,10 @@ using Tennis_Competitions.Data.Context;
 namespace Tennis_Competitions.Data.Migrations
 {
     [DbContext(typeof(TennisCompetitionsDBContext))]
-    partial class TennisCompetitionsDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220605211152_DeletedIdPropertyInTournament")]
+    partial class DeletedIdPropertyInTournament
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
