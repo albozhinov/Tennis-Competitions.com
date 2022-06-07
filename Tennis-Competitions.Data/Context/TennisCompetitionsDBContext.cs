a3f46cacd8e710963ca5831de5bb9cd5a0452f02
albozhinov <a.bozhinov90@gmail.com>
2022-06-07 21:35:38 +0300

    Added TournamentMatch - DbSet, Composite key and add json data logic.

--- Tennis-Competitions.Data/Context/TennisCompetitionsDBContext.cs
+++ Tennis-Competitions.Data/Context/TennisCompetitionsDBContext.cs
@@ -15,19 +15,19 @@
 
         public DbSet<PlayerMatch> PlayerMatches { get; set; }
 
+        public DbSet<TournamentMatch> TournamentMatches { get; set; }
+
 
         public TennisCompetitionsDBContext()
         {
 
         }
 
-
         public TennisCompetitionsDBContext(DbContextOptions<TennisCompetitionsDBContext> options)
             : base(options)
         {
 
         }
-
 
         private void LoadJsonInDB(ModelBuilder builder)
         {
@@ -38,18 +38,24 @@
                 var matchesAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\matches.json");
                 var playerMatchAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\playermatch.json");
                 var playerTournamentAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\playertournament.json");
+                var tournamentMatchAsJson = File.ReadAllText(@"..\Tennis-Competitions.Data\Jsons\tournamentmatch.json");
+
 
                 var tournaments = JsonConvert.DeserializeObject<Tournament[]>(tournamentsAsJson);
                 var players = JsonConvert.DeserializeObject<Player[]>(playersAsJson);
                 var matches = JsonConvert.DeserializeObject<Match[]>(matchesAsJson);
                 var playerMatches = JsonConvert.DeserializeObject<PlayerMatch[]>(playerMatchAsJson);
                 var playerTournaments = JsonConvert.DeserializeObject<PlayerTournament[]>(playerTournamentAsJson);
+                var tournamentMatches = JsonConvert.DeserializeObject<TournamentMatch[]>(tournamentMatchAsJson);
+
 
                 builder.Entity<Tournament>().HasData(tournaments);
                 builder.Entity<Player>().HasData(players);
                 builder.Entity<Match>().HasData(matches);
                 builder.Entity<PlayerMatch>().HasData(playerMatches);
                 builder.Entity<PlayerTournament>().HasData(playerTournaments);
+                builder.Entity<TournamentMatch>().HasData(tournamentMatches);
+
 
             }
             catch (Exception)
@@ -67,6 +73,8 @@
 
             builder.Entity<PlayerTournament>()
                             .HasKey(pt => new { pt.PlayerId, pt.TournamentId });
+
+            builder.Entity<TournamentMatch>().HasKey(tm => new { tm.MatchId, tm.TournamentId });
 
             base.OnModelCreating(builder);
         }
