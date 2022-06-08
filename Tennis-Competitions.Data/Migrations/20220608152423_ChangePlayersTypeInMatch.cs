bf32837dcec5306474d064ad9133f575c9be5f41
albozhinov <a.bozhinov90@gmail.com>
2022-06-08 18:26:02 +0300

    Create new Migration

---
+++ Tennis-Competitions.Data/Migrations/20220608152423_ChangePlayersTypeInMatch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tennis_Competitions.Data.Migrations
+{
+    public partial class ChangePlayersTypeInMatch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Matches_MatchId",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_MatchId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "MatchId",
+                table: "Players");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MatchId",
+                table: "Players",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_MatchId",
+                table: "Players",
+                column: "MatchId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Matches_MatchId",
+                table: "Players",
+                column: "MatchId",
+                principalTable: "Matches",
+                principalColumn: "Id");
+        }
+    }
+}
