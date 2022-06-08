48f386634e309a8f9b967f6e51ce07e5c782ae82
albozhinov <a.bozhinov90@gmail.com>
2022-06-09 00:00:56 +0300

    Added controllers

---
+++ Tennis-Competitions.com/Controllers/TournamentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tennis_Competitions.Services.Contracts;
+
+namespace Tennis_Competitions.com.Controllers
+{
+    public class TournamentController : Controller
+    {
+        private readonly ITournamentService tournamentService;
+
+        public TournamentController(ITournamentService _tournamentService)
+        {
+            tournamentService = _tournamentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tours = tournamentService.GetAllTournaments();
+
+            return View(tours);
+        }
+    }
+}
