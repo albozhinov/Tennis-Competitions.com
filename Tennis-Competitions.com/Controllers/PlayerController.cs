8bdb46310aeeef3548da759d2d2ba27f42bf1093
albozhinov <a.bozhinov90@gmail.com>
2022-06-11 15:18:00 +0300

    Implement Index action logic.

--- Tennis-Competitions.com/Controllers/PlayerController.cs
+++ Tennis-Competitions.com/Controllers/PlayerController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Tennis_Competitions.com.Controllers
+﻿namespace Tennis_Competitions.com.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Tennis_Competitions.Services.Contracts;
+
     public class PlayerController : Controller
     {
-        public IActionResult Index()
+        private readonly IPlayerService playerService;
+
+        public PlayerController(IPlayerService _playerService)
         {
-            return View();
+            playerService = _playerService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var players = await playerService.GetAllPlayers();
+
+            return View(players);
         }
     }
 }
